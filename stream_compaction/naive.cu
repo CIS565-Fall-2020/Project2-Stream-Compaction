4a1385b50113f117d7c0d57ca6b9484ca7e52d7b
JerryYan97 <yanjiarui97@126.com>
2020-09-17 19:42:04 -0400

    basic

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+int* dev_data_array1, * dev_data_array2;
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,14 +14,72 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+        __global__ void addition_process(int* dev_array1, int* dev_array2, const int d, const int n) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            int two_power_d_min_1 = powf(2, d - 1);
+            if (index >= two_power_d_min_1) {
+                dev_array2[index] = dev_array1[index - two_power_d_min_1] + dev_array1[index];
+            }
+            else {
+                dev_array2[index] = dev_array1[index];
+            }
+        }
+
+        __global__ void right_shift(int* dev_array1, int* dev_array2, const int n) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            if (index == 0) {
+                dev_array2[index] = 0;
+            }
+            else {
+                dev_array2[index] = dev_array1[index - 1];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            // Init all the requirement data:
+            int sizeInBytes = n * sizeof(int);
+            int blockSize = 128;
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            cudaMalloc((void**)&dev_data_array1, sizeInBytes);
+            checkCUDAError("cudaMalloc dev_data_array1 failed!");
+            cudaMalloc((void**)&dev_data_array2, sizeInBytes);
+            checkCUDAError("cudaMalloc dev_data_array2 failed!");
+            cudaMemcpy(dev_data_array1, idata, sizeInBytes, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_data_array1 failed!");
+            cudaMemcpy(dev_data_array2, idata, sizeInBytes, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_data_array2 failed!");
+
             timer().startGpuTimer();
             // TODO
+            // Inclusive scan:
+            int d_max = ilog2ceil(n);
+            for (int d = 1; d <= d_max; ++d)
+            {
+                addition_process <<< fullBlocksPerGrid, blockSize >>> (dev_data_array1, dev_data_array2, d, n);
+                checkCUDAError("Naive addition_process failed!");
+                int* temp = dev_data_array1;
+                dev_data_array1 = dev_data_array2;
+                dev_data_array2 = temp;
+            }
+            // Right shift to get an exclusive scan:
+            right_shift <<< fullBlocksPerGrid, blockSize >>> (dev_data_array1, dev_data_array2, n);
             timer().endGpuTimer();
+            cudaMemcpy(odata, dev_data_array2, sizeInBytes, cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy odata failed!");
+            cudaFree(dev_data_array1);
+            checkCUDAError("cudaFree(dev_data_array1) failed!");
+            cudaFree(dev_data_array2);
+            checkCUDAError("cudaFree(dev_data_array2) failed!");
         }
     }
 }
--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+#include <thrust/copy.h>
 #include <thrust/scan.h>
 #include "common.h"
 #include "thrust.h"
@@ -18,11 +19,19 @@ namespace StreamCompaction {
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            thrust::host_vector<int> host_i(idata, idata + n);
+            thrust::host_vector<int> host_o(n);
+            thrust::device_vector<int> dev_i = host_i;
+            thrust::device_vector<int> dev_o = host_o;
+
             timer().startGpuTimer();
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(dev_i.begin(), dev_i.end(), dev_o.begin());
             timer().endGpuTimer();
+
+            thrust::copy(dev_o.begin(), dev_o.end(), odata);
         }
     }
 }
