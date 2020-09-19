f1ff2ce14333a77dcb6eafbf82aa0efbb1808505
Gizem Dal <gizemdal@users.noreply.github.com>
2020-09-19 00:01:44 -0400

    Part 1 almost done

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -19,7 +19,15 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            for (int i = 0; i < n; ++i) {
+                int prefix_idx = i - 1;
+                if (prefix_idx < 0) {
+                    odata[i] = 0;
+                }
+                else {
+                    odata[i] = odata[i - 1] + idata[i - 1];
+                }
+            } 
             timer().endCpuTimer();
         }
 
@@ -30,9 +38,16 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int count = 0;
+            for (int i = 0; i < n; ++i) {
+                int elem = idata[i];
+                if (elem != 0) {
+                    odata[count] = elem;
+                    count++;
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -42,9 +57,32 @@ namespace StreamCompaction {
          */
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int* temp = new int[n]; // create temp array
+            // fill temp array with 0s and 1s
+            for (int i = 0; i < n; ++i) {
+                int elem = idata[i];
+                if (elem != 0) {
+                    temp[i] = 1;
+                }
+                else {
+                    temp[i] = 0;
+                }
+            }
+            // run scan
+            int* scanned = new int[n] {0};
+            StreamCompaction::CPU::scan(n, scanned, temp);
+            // scatter
+            int count = 0;
+            for (int i = 0; i < n; ++i) {
+                if (temp[i] == 1) {
+                    odata[scanned[i]] = idata[i];
+                    count++;
+                }
+            }
+            delete[] scanned;
+            delete[] temp;
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
     }
 }
