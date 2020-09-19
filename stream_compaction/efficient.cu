65b8cc22aab3eb5f43c67e064ba2fe2b026b1849
HAN YAN <hyan99@park.key.seas.upenn.edu>
2020-09-19 13:42:39 -0400

    efficient compact

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -83,6 +83,58 @@ namespace StreamCompaction {
 			}
 		}
 
+		__global__ void kernelEfficientCompact(int *g_odata, int *g_idata, int *g_sdata, int *g_bdata, int n, int N) {
+			int index = threadIdx.x;
+			// Build binary array
+			if (g_idata[index] == 0) {
+				g_bdata[index] = 0;
+			}
+			else {
+				g_bdata[index] = 1;
+			}
+			// Efficient scan
+			__syncthreads();
+			int offset = 2;
+			g_sdata[index] = g_bdata[index];
+			// up-sweep
+			for (int d = N / 2; d >= 1; d >>= 1) {
+				__syncthreads();
+				if (index < d) {
+					int a = n - 1 - (index * offset);
+					int b = a - offset / 2;
+					if (a >= 0 && b >= 0) {
+						g_sdata[a] += g_sdata[b];
+					}
+				}
+				offset *= 2;
+			}
+			// down-sweep
+			if (index == 0 && n > 0) {
+				g_sdata[n - 1] = 0;
+			}
+			offset /= 2;
+			for (int d = 1; d <= N / 2; d *= 2) {
+				__syncthreads();
+				if (index < d) {
+					int a = n - 1 - (index * offset);
+					int b = a - offset / 2;
+					if (a >= 0 && b >= 0) {
+						int tmp = g_sdata[b];
+						g_sdata[b] = g_sdata[a];
+						g_sdata[a] += tmp;
+					}
+				}
+				offset /= 2;
+			}
+			// Scatter
+			__syncthreads();
+			if (g_bdata[index] == 1) {
+				int idx = g_sdata[index];
+				g_odata[idx] = g_idata[index];
+			}
+		}
+
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
@@ -122,10 +174,43 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+			int k = ilog2ceil(n);
+			int N = (int)pow(2, k);
+
+			int *g_odata;
+			int *g_idata;
+			int *g_bdata;
+			int *g_sdata;
+			cudaMalloc((void**)&g_idata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc g_idata failed!");
+			cudaMalloc((void**)&g_odata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc g_odata failed!");
+			cudaMalloc((void**)&g_bdata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc g_bdata failed!");
+			cudaMalloc((void**)&g_sdata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc g_sdata failed!");
+
+			cudaMemcpy(g_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
             timer().startGpuTimer();
             // TODO
+			kernelEfficientCompact<<<1, n>>>(g_odata, g_idata, g_sdata, g_bdata, n, N);
+
             timer().endGpuTimer();
-            return -1;
+
+			// copy back output
+			int c1, c2;
+			cudaMemcpy(&c1, g_bdata + n - 1, sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(&c2, g_sdata + n - 1, sizeof(int), cudaMemcpyDeviceToHost);
+			int count = c1 + c2;
+			cudaMemcpy(odata, g_odata, sizeof(int) * count, cudaMemcpyDeviceToHost);
+
+			cudaFree(g_odata);
+			cudaFree(g_idata);
+			cudaFree(g_sdata);
+			cudaFree(g_bdata);
+
+            return count;
         }
     }
 }
