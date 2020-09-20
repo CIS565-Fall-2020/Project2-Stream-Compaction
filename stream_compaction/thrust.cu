eb7844b94869e9678ed44047b1843ea4bd4e591e
Jack12xl <jack12xieling@163.com>
2020-09-21 00:23:05 +0800

    add 4th part

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -17,12 +17,37 @@ namespace StreamCompaction {
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        void scan(int n, int *odata, const int *idata) {
+        void scan(int N, int *odata, const int *idata) {
+            if (N == 0) {
+                return;
+            }
+            assert(odata != nullptr);
+            assert(idata != nullptr);
+            int* dev_idata; int* dev_odata;
+
+            cudaMalloc((void**)&dev_idata, N * sizeof(int));
+            cudaMalloc((void**)&dev_odata, N * sizeof(int));
+            /*thrust::host_vector<int> thrust_hst_odata_vec = thrust*/
+
+            cudaMemcpy(dev_idata, idata, N * sizeof(int), cudaMemcpyHostToDevice);
+
+            thrust::device_ptr<int> dev_thrust_idata = thrust::device_pointer_cast(dev_idata);
+            //thrust::device_vector< int > dev_thrust_idata_vec(idata, idata + N);
+            thrust::device_vector<int> dev_thrust_idata_vec(dev_thrust_idata, dev_thrust_idata + N);
+
+            thrust::device_ptr<int> dev_thrust_odata = thrust::device_pointer_cast(dev_odata);
+            thrust::device_vector<int> dev_thrust_odata_vec(dev_thrust_odata, dev_thrust_odata + N);
+
             timer().startGpuTimer();
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(dev_thrust_idata_vec.begin(), dev_thrust_idata_vec.end(), dev_thrust_odata_vec.begin());
             timer().endGpuTimer();
+
+            thrust::copy(dev_thrust_odata_vec.begin(), dev_thrust_odata_vec.end(), odata);
+            /*cudaFree(dev_idata);
+            cudaFree(dev_odata);*/
         }
     }
 }
