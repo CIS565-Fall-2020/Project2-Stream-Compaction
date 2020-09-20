ed8a622772aa653a8330401894c7961f3bc84bb0
Szeyu Chan <1245577954@qq.com>
2020-09-20 20:12:30 +0800

    part3.2 Done

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -102,10 +102,71 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+
+			int *dev_bools;
+			int *dev_indices;
+			int *dev_idata;
+
+			int level = ilog2ceil(n);
+			int nPOT = 1 << level;	// Clamp n to power-of-two
+
+			cudaMalloc((void**)&dev_bools, nPOT * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc dev_bools failed!");
+			cudaMalloc((void**)&dev_indices, nPOT * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc dev_indices failed!");
+			cudaMalloc((void**)&dev_idata, nPOT * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc dev_idata failed!");
+
+			cudaMemset(dev_idata, 0, nPOT * sizeof(int));
+			cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+
             timer().startGpuTimer();
-            // TODO
+            // DONE
+			// Step1: Map idata to bools
+			dim3 blocksPerGridnPOT((nPOT + threadsPerBlock - 1) / threadsPerBlock);
+			Common::kernMapToBoolean << <blocksPerGridnPOT, threadsPerBlock >> > (nPOT, dev_bools, dev_idata);
+			cudaMemcpy(dev_indices, dev_bools, nPOT * sizeof(int), cudaMemcpyDeviceToDevice);
+			// Step2: Scan indices
+			// Up Sweep
+			int nSwept = nPOT;
+			for (int d = 0; d < level; ++d)
+			{
+				nSwept /= 2;
+				dim3 blocksPerGrid((nSwept + threadsPerBlock - 1) / threadsPerBlock);
+				int scaleIndex = 1 << (d + 1);
+				int offsetLeft = (1 << d) - 1;
+				int offsetRight = (1 << (d + 1)) - 1;
+				kernUpSweep << <blocksPerGrid, threadsPerBlock >> > (dev_indices, nSwept, scaleIndex, offsetLeft, offsetRight);
+			}
+			// Set root to zero
+			cudaMemset(dev_indices + nPOT - 1, 0, sizeof(int));
+			// Down Sweep
+			nSwept = 1;
+			for (int d = level - 1; d >= 0; --d)
+			{
+				dim3 blocksPerGrid((nSwept + threadsPerBlock - 1) / threadsPerBlock);
+				int scaleIndex = 1 << (d + 1);
+				int offsetLeft = (1 << d) - 1;
+				int offsetRight = (1 << (d + 1)) - 1;
+				kernDownSweep << < blocksPerGrid, threadsPerBlock >> > (dev_indices, nSwept, scaleIndex, offsetLeft, offsetRight);
+				nSwept *= 2;
+			}
+			// Step3: Scatter
+			Common::kernScatter << <blocksPerGridnPOT, threadsPerBlock >> > (nPOT, dev_idata, dev_idata, dev_bools, dev_indices);
+
             timer().endGpuTimer();
-            return -1;
+			cudaMemcpy(odata, dev_idata, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+			int lastIndex = 0;
+			cudaMemcpy(&lastIndex, dev_indices + nPOT - 1, sizeof(int), cudaMemcpyDeviceToHost);
+			int lastBool = 0;
+			cudaMemcpy(&lastBool, dev_bools + nPOT - 1, sizeof(int), cudaMemcpyDeviceToHost);
+
+			cudaFree(dev_bools);
+			cudaFree(dev_indices);
+			cudaFree(dev_idata);
+            return lastIndex + lastBool;
         }
     }
 }
