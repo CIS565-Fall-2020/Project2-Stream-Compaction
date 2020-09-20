8679957f38d730780097fcca38ad6de8921080ea
Szeyu Chan <1245577954@qq.com>
2020-09-20 18:58:13 +0800

    Part3.1 Done

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -11,14 +11,85 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
+		
+		// n: number of blocks that need to be swept
+		// scaleIndex: 2^(d + 1)
+		// offsetLeft: 2^(d) - 1
+		// offsetRight: 2^(d + 1) - 1
+		__global__ void kernUpSweep(int* oData, int nSwept, int scaleIndex, int offsetLeft, int offsetRight)
+		{
+			int index = blockIdx.x * blockDim.x + threadIdx.x;
+			if (index >= nSwept)
+			{
+				return;
+			}
+			int k = index * scaleIndex;
+			oData[k + offsetRight] += oData[k + offsetLeft];
+		}
+
+		// n: number of blocks that need to be swept
+		// scaleIndex: 2^(d + 1)
+		// offsetLeft: 2^(d) - 1
+		// offsetRight: 2^(d + 1) - 1
+		__global__ void kernDownSweep(int* oData, int nSwept, int scaleIndex, int offsetLeft, int offsetRight)
+		{
+			int index = blockIdx.x * blockDim.x + threadIdx.x;
+			if (index >= nSwept)
+			{
+				return;
+			}
+			int k = index * scaleIndex;
+			int t = oData[k + offsetLeft];
+			oData[k + offsetLeft] = oData[k + offsetRight];
+			oData[k + offsetRight] += t;
+		}
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+
+			int *dev_odata;
+			int level = ilog2ceil(n);
+			int nPOT = 1 << level;	// Clamp n to power-of-two
+			cudaMalloc((void**)&dev_odata, nPOT * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc dev_odata1 failed!");
+			cudaMemset(dev_odata, 0, nPOT * sizeof(int));
+			cudaMemcpy(dev_odata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+			
+
             timer().startGpuTimer();
-            // TODO
+            // DONE
+			
+			// Up Sweep
+			int nSwept = nPOT;
+			for (int d = 0; d < level; ++d)
+			{
+				nSwept /= 2;
+				dim3 blocksPerGrid((nSwept + threadsPerBlock - 1) / threadsPerBlock);
+				int scaleIndex = 1 << (d + 1);
+				int offsetLeft = (1 << d) - 1;
+				int offsetRight = (1 << (d + 1)) - 1;
+				kernUpSweep << <blocksPerGrid, threadsPerBlock >> > (dev_odata, nSwept, scaleIndex, offsetLeft, offsetRight);
+			}
+			// Set root to zero
+			cudaMemset(dev_odata + nPOT - 1, 0, sizeof(int));
+			// Down Sweep
+			nSwept = 1;
+			for (int d = level - 1; d >= 0; --d)
+			{
+				dim3 blocksPerGrid((nSwept + threadsPerBlock - 1) / threadsPerBlock);
+				int scaleIndex = 1 << (d + 1);
+				int offsetLeft = (1 << d) - 1;
+				int offsetRight = (1 << (d + 1)) - 1;
+				kernDownSweep << < blocksPerGrid, threadsPerBlock >> > (dev_odata, nSwept, scaleIndex, offsetLeft, offsetRight);
+				nSwept *= 2;
+			}
+
             timer().endGpuTimer();
+
+			cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+			cudaFree(dev_odata);
         }
 
         /**
