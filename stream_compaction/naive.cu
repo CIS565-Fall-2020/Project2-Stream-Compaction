c85522b4a2a47e83ba4300cc7ce30bcc6d65c7e8
Gizem Dal <gizemdal@users.noreply.github.com>
2020-09-20 19:15:17 -0400

    Part 2 done

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -1,7 +1,12 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <thrust/device_vector.h>
 #include "common.h"
 #include "naive.h"
+#include "thrust.h"
+#include <iostream>
+
+#define blockSize 128
 
 namespace StreamCompaction {
     namespace Naive {
@@ -11,15 +16,59 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+
+        __global__ void kernNaiveScan(int n, int d, int* in1, int* in2, int* out) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            int pow2 = int(powf(2, d - 1));
+            if (index >= pow2) {
+                out[index] = in1[index - pow2] + in1[index];
+            }
+            in2[index] = out[index];
+        }
+
+        __global__ void kernShiftArray(int n, int* in1, int* in2) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            if (index == 0) in2[index] = 0;
+            else {
+                in2[index] = in1[index - 1];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            // first allocate buffers and define kernel parameters
+            int* input;
+            int* input_temp;
+            int* output;
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+            cudaMalloc((void**)&input, n * sizeof(int));
+            cudaMalloc((void**)&input_temp, n * sizeof(int));
+            cudaMalloc((void**)&output, n * sizeof(int));
+            cudaMemcpy(input, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            cudaMemcpy(input_temp, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            cudaMemcpy(output, odata, sizeof(int) * n, cudaMemcpyHostToDevice);
             timer().startGpuTimer();
-            // TODO
+            // append identity to beginning and shift array
+            kernShiftArray <<< fullBlocksPerGrid, blockSize >> > (n, input, input_temp);
+            std::swap(input_temp, input);
+            // make ilog2ceil(n) kernel calls for scan
+            for (int d = 1; d <= ilog2ceil(n); ++d) {
+                kernNaiveScan<<<fullBlocksPerGrid, blockSize>>>(n, d, input, input_temp, output);
+                std::swap(input, input_temp);
+            }
             timer().endGpuTimer();
+            cudaMemcpy(odata, input, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            cudaFree(input);
+            cudaFree(input_temp);
+            cudaFree(output);
         }
     }
 }
